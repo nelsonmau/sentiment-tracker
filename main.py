d1da32b10adc037fb97c54c80406ca952d81edd5
Michael Brunton-Spall <michael@brunton-spall.co.uk>
2012-09-20 21:46:44 +0100

    add debug template as an embed template plus code

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from google.appengine.ext import webapp
-from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp import template
 
 import helpers
@@ -14,37 +13,33 @@ MIN=60
 HOUR=60*MIN
 
 
-try:
-    from django.utils import simplejson
-except ImportError:
-    try:
-        from django.utils import simplejson
-    except ImportError:
-        import json as simplejson
-        
+import json        
 import datetime
         
 class MainRequest(webapp.RequestHandler):
     @helpers.write_response
-    def get(self, *args):
-        pollname = args[0]
+    def get(self, pollname):
         poll = models.Poll.get_by_name(pollname)
         path = os.path.join(os.path.dirname(__file__), "templates", "main.html")
         return template.render(path, {'poll': poll})
 
 class DebugRequest(webapp.RequestHandler):
     @helpers.write_response
-    def get(self, *args):
-        pollname = args[0]
+    def get(self, pollname):
         poll = models.Poll.get_by_name(pollname)
         path = os.path.join(os.path.dirname(__file__), "templates", "debug.html")
         return template.render(path, {'poll': poll})
 
+class EmbedRequest(webapp.RequestHandler):
+    @helpers.write_response
+    def get(self, pollname):
+        poll = models.Poll.get_by_name(pollname)
+        path = os.path.join(os.path.dirname(__file__), "templates", "embed.html")
+        return template.render(path, {'poll': poll})
+
 class RegisterIncrRequest(webapp.RequestHandler):
     @helpers.write_response
-    def get(self, *args):
-        pollname = args[0]
-        choice = args[1]
+    def get(self, pollname, choice):
         callback = self.request.get('callback')                
         path = os.path.join(os.path.dirname(__file__), "templates", "register.html")
         poll = models.Poll.get_by_name(pollname)
@@ -58,9 +53,7 @@ class RegisterIncrRequest(webapp.RequestHandler):
 
 class RegisterDecrRequest(webapp.RequestHandler):
     @helpers.write_response
-    def get(self, *args):
-        pollname = args[0]
-        choice = args[1]
+    def get(self, pollname, choice):
         callback = self.request.get('callback')
         path = os.path.join(os.path.dirname(__file__), "templates", "register.html")
         poll = models.Poll.get_by_name(pollname)
@@ -103,6 +96,7 @@ class PersistVotesRequest(webapp.RequestHandler):
 app = webapp.WSGIApplication([
         ('/poll/([\w-]+)/display', MainRequest),
         ('/poll/([\w-]+)/debug', DebugRequest),
+        ('/poll/([\w-]+)/embed', EmbedRequest),
         # Results, in a format, like /results/json or /results/jsonp...
         ('/poll/([\w-]+)/results/(\w+)', ResultsRequest),
         # Post a value to counter
